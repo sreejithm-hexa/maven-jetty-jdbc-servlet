ef4b04205e70a1d17da31393e1e1569780e824ab
sreejithm-hexa <84178325+sreejithm-hexa@users.noreply.github.com>
2022-02-02 18:09:13 +0530

    Update V2__insert.sql

--- DB_script/V2__insert.sql
+++ DB_script/V2__insert.sql
@@ -1,5 +1,1 @@
-INSERT INTO CUSTOMER VALUES('kkrs@gmail.com', 'kkrs', 'kaustiv','MALE', 7278073227,'west bengal', 'kolkata', 5000);
-
-INSERT INTO FOOD_VENDOR VALUES('hmr@gmail.com', 'hmr', 'Canteen Management System', '7894561230', 'west bengal', 'NON-VEG','kolkata');
-
-NSERT INTO MENU VALUES(1, 'biryani', 150, 1, 'hmr@gmail.com', 241, '*****','Non-Veg');
+drop database mjjs;
