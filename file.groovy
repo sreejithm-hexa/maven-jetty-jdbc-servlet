13eae807a83975748dbe5852bbda77f14e6567e4
sreejithm-hexa <84178325+sreejithm-hexa@users.noreply.github.com>
2022-02-02 13:47:43 +0530

    Update file.groovy

--- file.groovy
+++ file.groovy
@@ -3,7 +3,7 @@ import groovy.io.FileType
 
 def fileList = []
 
-def dir = new File("DB_scripts")
+def dir = new File("DB_script")
 dir.eachFileRecurse (FileType.FILES) { file ->
   fileList << file
 }
